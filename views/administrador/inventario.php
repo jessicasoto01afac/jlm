48d469af8fa9991260c3f97135e747cbf909f1e8
jessicasoto01afac <jessica.soto928@gmail.com>
2022-05-02 00:50:30 -0500

    MAS CAMBIPOS

--- views/administrador/inventario.php
+++ views/administrador/inventario.php
@@ -22,7 +22,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="../template/img/logo.png" />
-    <title>JLM|Articulos</title>
+    <title>JLM|INVENTARIO</title>
     <!-- vendor css -->
     <link href="../template/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
     <link href="../template/lib/Ionicons/css/ionicons.css" rel="stylesheet">
