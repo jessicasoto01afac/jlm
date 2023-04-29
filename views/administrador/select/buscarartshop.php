59ff5be0abdaee1566fe70bb263f1cd61a5f8970
jessicasoto01afac <jessica.soto928@gmail.com>
2023-04-29 13:56:03 -0600

    cambios

--- views/administrador/select/buscarartshop.php
+++ views/administrador/select/buscarartshop.php
@@ -1,11 +1,11 @@
 <?php include ("../../controller/conexion.php");
-      $sql = "SELECT codigo_proveedor,descrip_proveedor FROM artproveedor a WHERE estado = 0 ORDER BY id_arprov ASC";
+      $sql = "SELECT id_articulo,codigo_proveedor,descrip_proveedor FROM artproveedor a WHERE estado = 0 ORDER BY id_arprov ASC";
       $articulo = mysqli_query($conexion,$sql);
     ?>
 			<select class="form-control select2-show-search" data-placeholder="Choose one (with searchbox)" onchange="desartic4()" id="mprvedd" name="mprvedd" type="text" data-live-search="true" style="width: 100%" >
 			<option value="0">CODIGO</option> 
 			<?php while($idpst = mysqli_fetch_row($articulo)):?>                      
-			<option value="<?php echo $idpst[0]?>"><?php echo $idpst[0]?></option>
+			<option value="<?php echo $idpst[0]?>"><?php echo $idpst[1]?></option>
 			<?php endwhile; ?>
 			</select>
 	<script type="text/javascript">
@@ -25,7 +25,7 @@
 //funcion para BUSCAR EL ARTICULO
 function desartic4(){
 //alert("eentraarticulo")
-var codico = "3700-19"; 
+var codico =  document.getElementById('mprvedd').value; 
 $.ajax({
           url: '../controller/php/conprvart.php',
           type: 'POST'
